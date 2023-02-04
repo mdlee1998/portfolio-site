e87087213e3da04fed51f655c84d7142ab5b099d
mdlee1998 <mdlee1998@gmail.com>
2023-02-04 17:06:52 -0500

    Initial commit
    Initial commit

---
+++ frontend/README.md
@@ -0,0 +1,1 @@
+This is a project to create a portfolio site for myself
---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <p>I am a website</p>
+    </div>
+  );
+}
+
+export default App;
