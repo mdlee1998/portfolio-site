c7e759730749eb9393b59d5102230e9aaf27856b
mdlee1998 <mdlee1998@gmail.com>
2023-02-22 23:59:26 -0500

    Section scrolling with mouse

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,47 @@
 import { Intro, Bio, Skills, Projects, Contact, Footer } from "./components";
+import { goToSection } from "./helpers";
 
+import { useDispatch, useSelector } from "react-redux";
+import { changeSection, stateSection } from "./slices/sectionSlice";
 
 function App() {
+
+  let sectionName = useSelector(stateSection);
+  const dispatch = useDispatch();
+
+  const goToNextSection = () => {
+    const app = document.querySelector('.App');
+    const section = app.querySelector(`[data-section=${sectionName}]`);
+    return goToSection({section, modifier: 'next'});
+  }
+
+  const goToPreviousSection = () => {
+    const app = document.querySelector('.App');
+    const section = app.querySelector(`[data-section=${app.dataset.current}]`);
+    return goToSection({section, modifier: 'previous'});
+  }
+
+  const handleMouseWheel = e => {
+    let newSection;
+    switch (Math.sign(e.deltaY)) {
+      case 1:
+        newSection = goToNextSection();
+        break;
+      case -1:
+        newSection = goToPreviousSection();
+        break;
+    }
+    setTimeout(() => {
+      dispatch(changeSection(newSection));
+      sectionName = newSection;
+    }, 200);
+  }
+
+    document.documentElement.addEventListener('wheel', handleMouseWheel, false);
+    document.documentElement.addEventListener('mousewheel', handleMouseWheel, false);
+
   return (
-    <div className="App">
+    <div className="App" data-current={sectionName}>
       <Intro name='intro'></Intro>
       <Bio name='bio'></Bio>
       <Skills name='skills'></Skills>
